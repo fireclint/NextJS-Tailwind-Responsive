bc395030ad6bb9a80d0c20dfe042b7432b58f45c
Bezalell Barnet W <bezbanz@gmail.com>
2024-02-05 10:41:51 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import Hero from '../components/Hero';
-import Instagram from '../components/Instagram';
 import Slider from '../components/Slider';
 import { SliderData } from '../components/SliderData';
 import AboutUsPage from '../components/AboutUs';
