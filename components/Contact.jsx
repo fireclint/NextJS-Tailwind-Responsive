d478749ac92877778d0dd97c732f2ccfe4dfca5e
Bezalell Barnet W <bezbanz@gmail.com>
2024-02-05 11:08:18 +0300

    update

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,10 +1,9 @@
-/* eslint-disable react/no-unescaped-entities */
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
     <div className='max-w-[1240px] m-auto p-4 h-screen'>
-        <h1 className='text-2xl font-bold text-center p-4'>Let's work together</h1>
+        <h1 className='text-2xl font-bold text-center p-4'>Let&rsquo;s work together</h1>
         <form className='max-w-[600px] m-auto'>
             <div className='grid grid-cols-2 gap-2'>
                 <input className='border shadow-lg p-3' type="text" placeholder='Name' />
@@ -15,7 +14,7 @@ const Contact = () => {
             <button className='border shadow-lg p-3 w-full mt-2'>Submit</button>
         </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
